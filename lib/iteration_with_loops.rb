be2ddec0a647a2d7d361db1c8b9bff690856c18e
Johndhee Ballesteros <johndheeballesteros13@gmail.com>
2021-01-01 00:37:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,13 +1,7 @@
 def join_nested_strings(src)
-new_array = 0
-row_index = 0
-while row_index < src.count do
-  element_index = 0
-  while element_index < src[row_index].count do
-    total += src[row_index][element_index]
-    element_index += 1
-  end
-  row_index += 1
-end
-total
-end
+new_array = []
+row_index = 0 
+while row_index < src.count do 
+  element_index = 0 
+  while element_index < src[row_index].count do 
+    if src
